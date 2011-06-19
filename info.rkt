11493f3c094ce9ec5e4a157e505a645de5ec0567
Danny Yoo <dyoo@cs.wpi.edu>
2011-06-19 15:21:39 -0400

    Re-releasing the package with fix to scribble documentation

--- info.rkt
+++ info.rkt
@@ -1,12 +1,13 @@
 #lang setup/infotab
 (define name "Simply Scheme")
 (define blurb '("Definitions to support programs written for Simply Scheme."))
-(define release-notes '((p "Ported to Racket 5.1.1")))
-(define version "2.0")
+(define release-notes '((p "Fix for the missing documentation link.")))
+(define version "2.1")
 (define categories '(misc))
 (define primary-file "main.rkt")
 (define required-core-version "5.1.1")
 (define repositories (list "4.x"))
+(define scribblings '(("manual.scrbl")))
 
 ;; Ignore these paths: they're here just for historical curiosity.
 (define compile-omit-paths (list "private" "make-distribution.rkt"))
